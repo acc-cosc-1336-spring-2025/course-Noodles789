ab9aef8d9916f183ac5b2c058d6dcfc4c112ee03
Noodles789 <derek.nguyen@g.austincc.edu>
2025-03-06 04:12:36 +0000

    run_tests.pysrc/homework/c_decisions/decisions.pytests/homework/c_decisions/tests_decisions.py

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,1 +1,18 @@
-#
+
+def get_options_ratio (options, total):
+    ratio = options / total
+    return ratio
+
+
+def get_faculty_rating(ratio):
+    if 0.9 <= ratio <= 1: 
+        return "Excellent"
+    if 0.8 <= ratio < 0.9:
+        return "Very Good"
+    if 0.7 <= ratio < 0.8:
+        return "Good"
+    if 0.6 <= ratio < 0.7:
+        return "Needs Improvement"
+    elif 0 <= ratio < 0.6: 
+        return "Unacceptable"
+    
